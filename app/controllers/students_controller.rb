bac234c521624be19009ea38ada5bdfda2752e19
mo-suke <konahour.a@gmail.com>
2020-04-20 21:54:39 +0900

    検索したときの並び順を整理

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,6 @@
 class StudentsController < ApplicationController
   def index
-    @students = Student.all
+    @students = Student.order(:grade, :school_class, :class_number)
   end
 
   def show
@@ -16,9 +16,22 @@ class StudentsController < ApplicationController
   end
 
   def create
+    @student = Student.new(params[:student])
+    if @student.save
+      redirect_to @student, notice: "児童を登録しました"
+    else
+      render "new"
+    end
   end
 
   def update
+    @student = Student.find(params[:id])
+    @student.assign_attributes(params[:student])
+    if @student.save
+      redirect_to @student, notice: "児童情報を更新しました"
+    else
+      render "edit"
+    end
   end
 
   def destroy
@@ -26,6 +39,7 @@ class StudentsController < ApplicationController
 
   def search
     @students = Student.search(params[:q])
+    @students.order(:grade).order(:school_class).order(:class_number)
     render "index"
   end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,7 @@
 class Student < ApplicationRecord
   class << self
     def search(query)
-      rel = order("class_number")
+      rel = order(:grade, :school_class, :class_number)
       if query.present?
         rel = rel.where("first_name LIKE ? OR last_name LIKE ?", "%#{query}%", "%#{query}%")
       end
